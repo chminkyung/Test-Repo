1671e25c6692f186ed825ec2f5d9dbc25e85bb2a
chminkyung <50662983+chminkyung@users.noreply.github.com>
2021-01-06 18:50:01 +0900

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff58701f4d20b0022058b0a",
+				"5ff58701f4d20b0022058b0c"
+			]
+		},
+		"5ff58701f4d20b0022058b0a": {
+			"id": "5ff58701f4d20b0022058b0a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff58701f4d20b0022058b0c": {
+			"id": "5ff58701f4d20b0022058b0c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
